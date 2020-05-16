0f2239ab6d33fd633637cf69dcce97b9e938e5d6
hhhhhq <Mr.j@tom.com>
2020-05-16 10:20:38 +0800

    clientValidate finish

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -2,42 +2,71 @@
  * @Author: hhhhhq
  * @Date: 2020-05-15 17:14:37
  * @LastEditors: hhhhhq
- * @LastEditTime: 2020-05-15 22:00:07
+ * @LastEditTime: 2020-05-16 10:15:26
  * @Description: file content
  */ 
 import React, { Component } from 'react'
+import classnames from 'classnames'
 // import PropTypes from 'prop-types'
 
 class GameForm extends Component{
   state = {
     title: '',
-    cover: ''
+    cover: '',
+    errors: {}, // 存放验证错误信息
   }
   
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    let errors = {}
+    if(this.state.title === '') errors.title = "Can't be empty"
+    if(this.state.cover === '') errors.cover = "Can't be empty"
+    this.setState({ errors })
+  } 
+
+  handleChange = (e) => {
+    if(!!this.state.errors[e.target.name]) {
+      let errors = Object.assign({}, this.state.errors)
+  
+      delete errors[e.target.name]
+      this.setState({
+        [e.target.name]: e.target.value,
+        errors
+      })
+    } else {
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }
+  }
+
   render() {
     return (
       <div>
-        <form className="ui form">
+        <form className="ui form" onSubmit={ this.handleSubmit }>
           <h1>Add new game</h1>
   
-          <div className="field">
+          <div className={ classnames('field', { error: !!this.state.errors.title }) }>
             <label htmlFor="title">Title</label>
             <input 
               type="text"
               name="title"
               value={ this.state.title }
-              onChange={ e => this.setState({ title: e.target.value })}
+              onChange={ this.handleChange }
             />
+            <span>{ this.state.errors.title }</span>
           </div>
   
-          <div className="field">
-            <label htmlFor="title">Cover</label>
+          <div className={ classnames('field', { error: !!this.state.errors.cover }) }>
+            <label htmlFor="cover">Cover</label>
               <input 
                 type="text"
-                name="text"
+                name="cover"
                 value={ this.state.cover }
-                onChange={ e => this.setState({ cover: e.target.value })}
+                onChange={ this.handleChange }
               />
+            <span>{ this.state.errors.cover }</span>
           </div>
 
           <div className="field">
